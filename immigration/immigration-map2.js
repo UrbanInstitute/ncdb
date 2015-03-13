6d4d91f2c592d8403a3900f1530a442d86f83fd2
Tim Meko <timmeko@gmail.com>
2015-03-13 14:28:18 -0400

    added interactive legend and styling

--- immigration/immigration-map2.js
+++ immigration/immigration-map2.js
@@ -38,53 +38,53 @@ $(document).ready(function () {
     var control = document.getElementById('layers');
 
     // Add a play button div
-        var play_button = control.appendChild(document.createElement('a'))
-        var pause = "&#9616;&#9616;";
-        var play = "&#9654;";
-        play_button.innerHTML = pause;
-        play_button.id = "play_button";
-        play_button.onclick = function () {
-            if (nextInterval) {
-                nextInterval = clearInterval(nextInterval);
-                play_button.innerHTML = play;
-            } else {
-                highlightLayer(i++);
-                if (i === 12) i = 0;
-                nextInterval = animate();
-                play_button.innerHTML = pause;
-            }
+    var play_button = control.appendChild(document.createElement('a'))
+    var pause = "&#9616;&#9616;";
+    var play = "&#9654;";
+    play_button.innerHTML = pause;
+    play_button.id = "play_button";
+    play_button.onclick = function () {
+        if (nextInterval) {
+            nextInterval = clearInterval(nextInterval);
+            play_button.innerHTML = play;
+        } else {
+            highlightLayer(i++);
+            if (i === 12) i = 0;
+            nextInterval = animate();
+            play_button.innerHTML = pause;
         }
+    }
 
-        layers.forEach(function (layer, n) {
-            layer.button = control.appendChild(document.createElement('a'));
-            layer.button.innerHTML = layers[n].name;
-            layer.button.onclick = function () {
-                highlightLayer();
-                i = n
-                nextInterval = clearInterval(nextInterval);
-                play_button.innerHTML = play;
-            };
-        });
+    layers.forEach(function (layer, n) {
+        layer.button = control.appendChild(document.createElement('a'));
+        layer.button.innerHTML = layers[n].name;
+        layer.button.onclick = function () {
+            highlightLayer();
+            i = n
+            nextInterval = clearInterval(nextInterval);
+            play_button.innerHTML = play;
+        };
+    });
 
 
     // we use a layer group to make it simple to remove an existing overlay
     // and add a new one in the same line of code, as below, without juggling
     // temporary variables.
-        var layerGroup = L.layerGroup().addTo(map);
+    var layerGroup = L.layerGroup().addTo(map);
 
     // i is the number of the currently-selected layer: this loops through
     // 0, 1, and 2.
-        var i = 0;
+    var i = 0;
 
     // show the first overlay as soon as the map loads
-        highlightLayer(i++);
+    highlightLayer(i++);
 
-        var nextInterval = animate();
+    var nextInterval = animate();
 
 
     function animate() {
         var i = 0
-        // and then time the next() function to run every 1 seconds
+            // and then time the next() function to run every 1 seconds
         return setInterval(function () {
             highlightLayer();
             if (++i >= layers.length) i = 0;
@@ -105,9 +105,12 @@ $(document).ready(function () {
 
         var active = control.getElementsByClassName('active');
         var activeLayer = layers[i].grid
-        activeLayer.on("mousemove",function(o){
-            if(o.data){
+         var defaultlegend = document.getElementById('legend');
+        activeLayer.on("mousemove", function (o) {
+            if (o.data) {
                 drawKey(o.data, i)
+            }else{
+               defaultlegend.innerHTML = "<img class='key' src='key/ncdb-key.png'/>";   
             }
         })
         for (var j = 0; j < active.length; j++) active[j].className = '';
@@ -115,24 +118,37 @@ $(document).ready(function () {
 
     }
 
-    function drawKey(data, index){
-        var legend = document.getElementById('legend')
-        switch(String(index)){
-            case "0":
-                if(typeof(data.SES9) != "undefined"){
-                    $(legend).text = "SES: " + data.SES9 + "<br/>Share bin: " + data.key9 + "<br/>Share: " + data.share90
-                }
-                break
-            case "1":
-                if(typeof(data.SES0) != "undefined"){
-                    legend.innerHTML = "SES: " + data.SES0 + "<br/>Share bin: " + data.key0 + "<br/>Share: " + data.share00
-                }
-                break
-            case "2":
-                if(typeof(data.SES1A) != "undefined"){
-                    legend.innerHTML = "SES: " + data.SES1A + "<br/>Share bin: " + data.key1a + "<br/>Share: " + data.share10
-                }
-                break
+    function drawKey(data, index) {
+        var SES;
+        var legend = document.getElementById('legend');
+        var legendtext = document.getElementById('legend-text');
+        switch (String(index)) {
+        case "0":
+            if (typeof (data.SES9) != "undefined") {
+                legend.innerHTML = "<img class='key' src='key/" + data.SES9 + "/" + data.key9 + ".jpg'/>";
+                legendtext.innerHTML = "<div id='year'>1990</div><div id='SES-label'>SES: " + data.SES90t + "</div><div id='immig-label'>Share of immigrants: " + data.share90 + "%</div>"
+
+
+            }
+            break
+        case "1":
+            if (typeof (data.SES0) != "undefined") {
+                legend.innerHTML = "<img class='key' src='key/" + data.SES0 + "/" + data.key0 + ".jpg'/>";
+                legendtext.innerHTML = "<div id='year'>2000</div><div id='SES-label'>SES: " + data.SES00t + "</div><div id='immig-label'>Share of immigrants: " + data.share00 + "%</div>"
+
+
+            }
+            break
+        case "2":
+            if (typeof (data.SES1A) != "undefined") {
+
+                legend.innerHTML = "<img class='key' src='key/" + data.SES1A + "/" + data.key1a + ".jpg'/>";
+                legendtext.innerHTML = "<div id='year'>2010</div><div id='SES-label'>SES: " + data.SES10tim + "</div><div id='immig-label'>Share of immigrants: " + data.share10 + "%</div>"
+
+
+            }
+            break
+            
         }
     }
 
