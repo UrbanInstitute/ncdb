13e04b3648cd97721d06cb6faf450c83d981e121
Tim Meko <timmeko@gmail.com>
2015-03-25 13:06:13 -0400

    housing calculator map style updates but breaks functionality

--- housing/calculator/js/main.js
+++ housing/calculator/js/main.js
@@ -154,88 +154,143 @@ function drawMap() {
         }];
 
 
-    var control = document.getElementById('layers');
+            var control = document.getElementById('layers');
 
-
-    var gridLayer = L.mapbox.gridLayer('urbaninstitute.tekrcnmi'); //1980
-    map.addLayer(gridLayer);
-    map.addControl(L.mapbox.gridControl(gridLayer));
-
-    // Add a play button div
-    var play_button = control.appendChild(
-      document.createElement('a')
-    );
-
-    var pause = "&#9616;&#9616;";
-    var play = "&#9654;";
-
-    play_button.innerHTML = pause;
-    play_button.id = "play_button";
-    play_button.onclick = function () {
-        if (nextInterval) {
-            nextInterval = clearInterval(nextInterval);
-            play_button.innerHTML = play;
-        } else {
-            highlightLayer(i++);
-            if (i === 12) i = 0;
-            nextInterval = animate();
+            // Add a play button div
+            var play_button = control.appendChild(document.createElement('a'))
+            var pause = "&#9616;&#9616;";
+            var play = "&#9654;";
             play_button.innerHTML = pause;
-        }
-    };
+            play_button.id = "play_button";
+            play_button.onclick = function () {
+                if (nextInterval) {
+                    nextInterval = clearInterval(nextInterval);
+                    play_button.innerHTML = play;
+                } else {
+                    highlightLayer(i++);
+                    if (i === 3) {
+                        i = 0
+                    };
+                    nextInterval = animate();
+                    play_button.innerHTML = pause;
+                }
+            }
 
-    layers.forEach(function (layer, n) {
-
-        layer.button = control.appendChild(document.createElement('a'));
-        layer.button.innerHTML = layers[n].name;
-        layer.button.onclick = function () {
-            layerGroup.clearLayers();
-            highlightLayer(n);
-            i = n;
-            nextInterval = clearInterval(nextInterval);
-            play_button.innerHTML = play;
-        };
-    });
+            layers.forEach(function (layer, n) {
+                layer.button = control.appendChild(document.createElement('a'));
+                layer.button.innerHTML = layers[n].name;
+                layer.button.onclick = function () {
+                    i = n
+                    highlightLayer();
+                    nextInterval = clearInterval(nextInterval);
+                    play_button.innerHTML = play;
+                };
+            });
 
 
-    // we use a layer group to make it simple to remove an existing overlay
-    // and add a new one in the same line of code, as below, without juggling
-    // temporary variables.
-    var layerGroup = L.layerGroup().addTo(map);
+            // we use a layer group to make it simple to remove an existing overlay
+            // and add a new one in the same line of code, as below, without juggling
+            // temporary variables.
+            var layerGroup = L.layerGroup().addTo(map);
 
-    // i is the number of the currently-selected layer: this loops through
-    // 0, 1, and 2.
-    var i = 0;
+            // i is the number of the currently-selected layer: this loops through
+            // 0, 1, and 2.
+            var i = 0;
 
-    // show the first overlay as soon as the map loads
-    highlightLayer(i++);
+            // show the first overlay as soon as the map loads
+            highlightLayer(i++);
 
-    var nextInterval = animate();
+            var nextInterval = animate();
 
-    function animate() {
-        // and then time the next() function to run every 1 seconds
-        return setInterval(function () {
-            highlightLayer(i);
-            if (++i >= layers.length) i = 0;
-        }, 3000 * 1);
 
-    }
+            function animate() {
+                // var i = 0
+                // and then time the next() function to run every 1 seconds
+                return setInterval(function () {
+                    highlightLayer();
+                    if (++i >= layers.length) i = 0;
+                }, 3000 * 1);
+            }
 
-    function highlightLayer(i) {
-        if (i === 0) {
-            layerGroup.clearLayers();
-        }
-        layerGroup.addLayer(layers[i].layer);
-        var active = control.getElementsByClassName('active');
-        for (var j = 0; j < active.length; j++) active[j].className = '';
-        layers[i].button.className = 'active';
+            // var active;
+            function highlightLayer() {
+                i = i % 3
+                if (i == 0) {
+                    layerGroup.clearLayers();
+                }
+                layerGroup.addLayer(layers[i].layer);
+                var gridControl = L.mapbox.gridControl(layers[i].grid);
+                map.addLayer(layers[i].grid);
 
-    }
+                var count = document.getElementById('count');
 
-    //streets on top
-    var streetLayer = L.mapbox.tileLayer('urbaninstitute.h5b1kc2b');
-    streetLayer
-        .setZIndex(100)
-        .addTo(map);
+
+                var active = control.getElementsByClassName('active');
+                var activeLayer = layers[i].grid
+                var defaultlegend = document.getElementById('legend');
+                activeLayer.on("mousemove", function (o) {
+                    if (o.data) {
+                        drawKey(o.data, i)
+                    } else {
+                        //           defaultlegend.innerHTML = "<img class='key' src='key/ncdb-key.png'/>";   
+                    }
+                })
+                for (var j = 0; j < active.length; j++) active[j].className = '';
+                layers[i].button.className = 'active';
+
+            }
+
+            function drawKey(data, index) {
+                var RentalBurden;
+                //var legend = document.getElementById('legend');
+                var legendtext = document.getElementById('legend-text');
+                 console.log(data);
+                //d3.selectAll(".selected").classed("selected", false)
+                var el;
+                
+                
+                switch (String(index)) {
+                case "0":
+                    if (typeof (data.SES9) != "undefined") {
+                        //el = d3.select(".ses" + data.SES9 + ".immig" + data.key9)
+                        //el.classed("selected", true)
+                        //el[0][0].parentNode.appendChild(el[0][0])
+                        legendtext.innerHTML = "<div id='year'>1990</div><div class='key-label'>Tract SES :: <span class='key-data'>" + data.SES90t + "</span></div><div class='key-label'>Share of immigrants :: <span class='key-data'>" + data.share90 + "%</span></div>"
+
+                    }
+
+                    break
+                case "1":
+                    if (typeof (data.SES0) != "undefined") {
+                       // el = d3.select(".ses" + data.SES0 + ".immig" + data.key0)
+                       // el.classed("selected", true)
+                       // el[0][0].parentNode.appendChild(el[0][0])
+                        legendtext.innerHTML = "<div id='year'>2000</div><div class='key-label'>Tract SES :: <span class='key-data'>" + data.SES00t + "</span></div><div class='key-label'>Share of immigrants :: <span class='key-data'>" + data.share00 + "%</span></div>"
+                    }
+                    break
+                case "2":
+                    if (typeof (data.SES1A) != "undefined") {
+                       // el = d3.select(".ses" + data.SES1A + ".immig" + data.key1a)
+                       // el.classed("selected", true)
+                       // el[0][0].parentNode.appendChild(el[0][0])
+                        legendtext.innerHTML = "<div id='year'>2010</div><div class='key-label'>Tract SES :: <span class='key-data'>" + data.SES10tim + "</span></div><div class='key-label'>Share of immigrants :: <span class='key-data'>" + data.share10 + "%</span></div>"
+                    }
+                    break
+
+                }
+                // el.classed("selected",true)
+                // el[0][0].parentNode.appendChild(el[0][0])
+            }
+
+            //streets on top
+            var streetLayer = L.mapbox.tileLayer('urbaninstitute.h5b1kc2b');
+            streetLayer
+                .setZIndex(100)
+                .addTo(map);
+
+    
+    
+///TIM DON'T TOUCH ANYTHING BELOW THIS
 
     $('#show-map, #view-burden-in-map').click(function() {
       if (currentState.lon !== null && currentState.lat !== null) {
